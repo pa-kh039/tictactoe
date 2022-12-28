e92e50a289428e289c89327740f0864395dcb596
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2022-12-28 23:39:33 +0530

    added states and events to enable x and o marks

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,63 @@
 // get the below snippet using rafce
-import React from 'react';
+import React, { useState } from 'react';
 
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setisXNext]=useState(false)
+  // console.log(board);
+  const handleSquareClick = (position) => { 
+    if (board[position])
+    {
+      return; //doing nothing if the square is already filled
+    }
+    setBoard((prev) => {
+      return prev.map( (square,pos)=>{
+        if(pos===position)
+        {
+          if (isXNext===true){
+              setisXNext(false);
+              return 'X';
+          }
+          else {
+            setisXNext(true);
+            return 'O';
+          }
+        }
+        return square;
+      })
+    }); 
+  };
+  
+  const renderSquare = (position) => {
+    return (
+      <Square
+        value={board[position]}
+        onClickF={() => handleSquareClick(position) }
+      />
+    );
+  };
+
   return (
-    <div className='board'>
-      <div className='board-row'>
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+    <div className="board">
+      <div className="board-row">
+        {/* <Square value={board[0]} onClick={handleSquareClick}/> */}
+        {/* the above wont work as onclick function will be immediately executed, so do the following */}
+        {/* <Square value={board[0]} onClick={()=>{handleSquareClick(0)}}/> */}
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-      <div className='board-row'>
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
-      <div className='board-row'>
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 // import './Square.style.css';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClickF }) => {
   // console.log(props);
-  return <button type="button" className='square'>{value}</button>;
+  return <button type="button" className='square' onClick={onClickF}>
+    {value}
+    </button>;
 };
 
 export default Square;
